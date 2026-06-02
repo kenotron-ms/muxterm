a6919284e15cc4a964fb43421a7af08f067deea9
Ken <ken@localhost>
2026-06-02 03:08:45 +0000

    feat(service): render sessiond systemd unit; web unit Wants/After it

--- internal/service/commander.go
+++ internal/service/commander.go
@@ -14,6 +14,11 @@ func SystemdUnitPath() string {
 	return filepath.Join(home, ".config", "systemd", "user", "muxterm.service")
 }
 
+func SessiondSystemdUnitPath() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".config", "systemd", "user", "muxterm-sessiond.service")
+}
+
 func LaunchdPlistPath() string {
 	home, _ := os.UserHomeDir()
 	return filepath.Join(home, "Library", "LaunchAgents", "com.muxterm.plist")
